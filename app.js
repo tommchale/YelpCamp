a0bb478cf6d4ea32134b49487a1470612fbda874
Tom McHale <tom.mchale@icloud.com>
2020-12-10 12:29:25 +0000

    add start script

--- app.js
+++ app.js
@@ -171,8 +171,10 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Serving on port 3000... :D");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serving on port ${port} :D`);
 });
 
 // Models - Views - Controller
