a6efac15e8a78b0b624dcbb98a8024e5636f30cb
Tom McHale <tom.mchale@icloud.com>
2020-12-13 12:39:17 +0000

    add mongo cloud seeding changes

--- models/campground.js
+++ models/campground.js
@@ -1,69 +1,70 @@
-const mongoose = require('mongoose')
-const Review = require('./review')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Review = require("./review");
+const Schema = mongoose.Schema;
 
 // defining a one to many relationship as there is the possiblility for a campground to have thousands of reviews
 // https://res.cloudinary.com/db2zsdvyh/image/upload/w_300/v1607103615/YelpCamp/fkjcgycju9nvsio0jtjm.jpg
 
 const ImageSchema = new Schema({
-    url: String,
-    filename: String
-})
+  url: String,
+  filename: String,
+});
 
-ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200')
-})
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload/w_450,h_300", "/upload/w_200");
+});
 
-const opts = { toJSON : {virtuals: true } }
+const opts = { toJSON: { virtuals: true } };
 
-const CampgroundSchema = new Schema({
+const CampgroundSchema = new Schema(
+  {
     title: String,
     images: [ImageSchema],
     geometry: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
     price: Number,
     description: String,
     location: String,
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
-}, opts)
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
 
 // to get information to show in map - need to create a virtual property in the schema, enable that schema to show through mongoose, then refer to it in the map
 
-CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
-    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
-            <p>${this.description.substring(0,30)}...</p>`
-})
-
-
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+            <p>${this.description.substring(0, 30)}...</p>`;
+});
 
 // findOneAndDelete is a query middleware called by the findByIdAndDelete mongo call
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        // This is removing review from review db where id was in the campground reviews array
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    // This is removing review from review db where id was in the campground reviews array
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-module.exports = mongoose.model('Campground', CampgroundSchema)
+module.exports = mongoose.model("Campground", CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,64 +1,68 @@
 // want seed files to be self contained
+if (process.env.NODE !== "production") {
+  require("dotenv").config();
+}
 
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 // . means back out one
-const Campground = require('../models/campground');
+const Campground = require("../models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 // function to return random element of that array
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    // clear the database 
-    await Campground.deleteMany({});
-    for (let i = 0; i < 150; i++) {
-        const randomNum = Math.floor(Math.random() * 443);
-        const price = Math.floor(Math.random() * 20) + 10
-        const camp = new Campground({
-            author: '5fc7d67c485731150d35816b',
-            location: `${cities[randomNum].city}, ${cities[randomNum].country}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            geometry: {
-                type: 'Point',
-                coordinates: [
-                    cities[randomNum].lng,
-                    cities[randomNum].lat,
-                ]
-            },
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/db2zsdvyh/image/upload/v1607463238/YelpCamp/hzgtr0mdkgewo6jusugu.jpg',
-                    filename: 'YelpCamp/hzgtr0mdkgewo6jusugu'
-                },
-                {
-                    url: 'https://res.cloudinary.com/db2zsdvyh/image/upload/v1607463241/YelpCamp/atazxid0rx9jfapvfp5j.jpg',
-                    filename: 'YelpCamp/atazxid0rx9jfapvfp5j'
-                }
-            ],
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga ratione nostrum voluptas minus corporis ad atque itaque ex reiciendis recusandae a sint blanditiis, accusantium voluptatem distinctio omnis delectus. Culpa, similique?',
-            price: price
-        })
-        await camp.save();
-    }
-}
+  // clear the database
+  await Campground.deleteMany({});
+  for (let i = 0; i < 150; i++) {
+    const randomNum = Math.floor(Math.random() * 443);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "5fd1e2ab1cbcd189410ecfb8",
+      location: `${cities[randomNum].city}, ${cities[randomNum].country}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      geometry: {
+        type: "Point",
+        coordinates: [cities[randomNum].lng, cities[randomNum].lat],
+      },
+      images: [
+        {
+          url:
+            "https://res.cloudinary.com/db2zsdvyh/image/upload/w_450,h_300/v1607605801/YelpCamp/dkmzcwgbwukurqeo52ph.jpg",
+          filename: "YelpCamp/dkmzcwgbwukurqeo52ph",
+        },
+        {
+          url:
+            "https://res.cloudinary.com/db2zsdvyh/image/upload/w_450,h_300/v1607635130/YelpCamp/mg7p1zxumyqtee0pjvm8.jpg",
+          filename: "YelpCamp/mg7p1zxumyqtee0pjvm8",
+        },
+      ],
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga ratione nostrum voluptas minus corporis ad atque itaque ex reiciendis recusandae a sint blanditiis, accusantium voluptatem distinctio omnis delectus. Culpa, similique?",
+      price: price,
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    // when your done should close the database
-    mongoose.connection.close();
-})
+  // when your done should close the database
+  mongoose.connection.close();
+});
